290c7a625c27a1755e60dc7ba19845eecce09515
Fred <fred.mny@gmail.com>
2020-12-22 12:05:22 -0300

    Small changes in the top bar: Adding the discord icon and changing icon order

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -120,8 +120,8 @@ group_names=[("  ",{'layout': def_layout, 'spawn':'termite'}),
            ("  ",{'layout': def_layout,'spawn':'firefox'}),
            ("  ",{'layout': def_layout}), 
            ("  ",{'layout': def_layout}),
-           (" 6 ",{'layout': def_layout}),
            ("  ",{'layout': def_layout, 'spawn':'keepassxc'}),
+           ("  ",{'layout': def_layout}),
            ("  ",{'layout': def_layout, 'spawn':'telegram-desktop'}),
            ("  ",{'layout': def_layout, 'spawn':'spotify'})]
 
