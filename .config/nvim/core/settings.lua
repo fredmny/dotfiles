2cea7d2d6fffbbed5ce5c3e3d94d33db1c1b0354
Fred <fred.mny@gmail.com>
2023-08-13 22:03:05 -0300

    start configuring nvim

---
+++ .config/nvim/core/settings.lua
@@ -0,0 +1,52 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.opt.cursorline = true
+vim.opt.autoread = true
+vim.opt.showcmd = true
+
+-- Better editor UI
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.numberwidth = 3
+vim.o.signcolumn = "yes"
+
+-- Number of screen lines to keep above and below the cursor
+vim.o.scrolloff = 8
+
+-- Better editing experience
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+vim.opt.smarttab = true
+--vim.opt.cindent = true
+vim.opt.autoindent = true
+vim.opt.wrap = true
+vim.opt.textwidth = 300
+
+
+-- Makes neovim and host OS clipboard play nicely with each other
+vim.o.clipboard = "unnamedplus"
+
+-- Case insensitive searching UNLESS /C or capital in search
+vim.o.ignorecase = true
+-- use case sensitiveness with <leader>c
+vim.o.smartcase = true
+
+-- Undo and backup options
+vim.o.backup = false
+vim.o.writebackup = false
+vim.o.undofile = true
+vim.o.swapfile = false
+
+-- Better buffer splitting
+--vim.o.splitright = true
+--vim.o.splitbelow = true
+
+-- Use mouse on all modes
+--vim.opt.mouse = "a"
+
+
+
+vim.cmd [[ set noswapfile ]]
