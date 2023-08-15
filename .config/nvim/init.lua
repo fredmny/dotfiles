ffc116ee97cb6c012386500bf766dbfb737026c3
Fred <fred.mny@gmail.com>
2023-08-15 09:32:21 -0300

    feat: further config nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,4 @@
 require("core.settings")
+require("core.keymaps")
+require("core.plugins")
+require("core.plugin_config")
---
+++ .config/nvim/lua/core/plugin_config/treesitter.lua
@@ -0,0 +1,11 @@
+require'nvim-treesitter.configs'.setup {
+-- A list of parser names, or "all"
+ensure_installed = { "c", "lua", "python", "sql", "vim" },
+
+-- Install parsers synchronously (only applied to `ensure_installed`)
+sync_install = false,
+auto_install = true,
+highlight = {
+  enable = true,
+},
+}
