3993ebd6122a6421bdc7affc877b3269a39b9119
Fred Waldow <fred.mny@gmail.com>
2023-12-05 17:27:24 -0300

    add and update nvim plugins

--- .config/nvim/lua/core/plugin_config/lspconfig.lua
+++ .config/nvim/lua/core/plugin_config/lspconfig.lua
@@ -1,6 +1,6 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-  ensure_installed = { "lua_ls", "pyright" }
+  ensure_installed = { "lua_ls", "pyright", "sqlls"}
 })
 
 require("lspconfig").lua_ls.setup {}
--- .config/nvim/lua/core/plugins.lua
+++ .config/nvim/lua/core/plugins.lua
@@ -29,7 +29,8 @@ return require('packer').startup(function(use)
   use 'williamboman/mason.nvim'
   use 'williamboman/mason-lspconfig.nvim'
   use 'neovim/nvim-lspconfig'
-
+  -- For which-key
+  use "folke/which-key.nvim"
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
