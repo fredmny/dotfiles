c4a5e19919a9ca08900dcacb15c810588616ff79
Fred <fred.mny@gmail.com>
2023-12-23 16:53:20 -0300

    screen layout

--- .screenlayout/standard.sh
+++ .screenlayout/standard.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --mode 2560x1440 --pos 1920x0 --rotate normal --output DP2 --off --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off --output HDMI-1-3 --off
+#xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --mode 2560x1440 --pos 1920x0 --rotate normal --output DP2 --off --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off --output HDMI-1-3 --off
+xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP2 --off --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off --output HDMI-1-3 --off
