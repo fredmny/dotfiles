0d05251fa6b4df6cf12887ca5985995e0604b5f7
Fred <fred.mny@gmail.com>
2024-04-09 12:48:21 -0300

    nvim add none-ls and completions configuration

---
+++ .config/nvim/lua/core/plugin_config/none-ls.lua
@@ -0,0 +1,16 @@
+local null_ls = require("null-ls")
+
+null_ls.setup({
+	sources = {
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.diagnostics.sqlfluff,
+		null_ls.builtins.formatting.sqlfluff,
+		null_ls.builtins.formatting.prettier,
+		-- null_ls.builtins.diagnostics.eslint_d,
+		null_ls.builtins.formatting.black,
+		null_ls.builtins.formatting.isort,
+		null_ls.builtins.formatting.yamlfmt,
+		null_ls.builtins.diagnostics.yamllint,
+		-- null_ls.builtins.completion.spell,
+	},
+})
