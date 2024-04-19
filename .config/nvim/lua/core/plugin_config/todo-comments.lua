5fdaf9cf3de14dc29620336f793503b1d51ac271
Fred Waldow <fred.waldow@trustly.com>
2024-04-19 13:26:26 -0300

    nvim: add personalized todo comment - remove

--- .config/nvim/lua/core/plugin_config/todo-comments.lua
+++ .config/nvim/lua/core/plugin_config/todo-comments.lua
@@ -1,3 +1,6 @@
-require('todo-comments').setup{
-    keywords =  { Q = { icon = " ", color = "#e26fe2" }}
-}
+require("todo-comments").setup({
+	keywords = {
+		Q = { icon = " ", color = "#e26fe2" },
+		REMOVE = { icon = " ", color = "#c8b966", alt = { "REM", "X" } },
+	},
+})
