cb746ee96a68a2704c7272317f79b4f4aac91aac
Fred <fred.mny@gmal.com>
2025-04-26 13:37:55 -0300

    feat: Add mappings for Lspsaga commands

--- .config/nvim/lua/plugins/config/lspsaga.lua
+++ .config/nvim/lua/plugins/config/lspsaga.lua
@@ -9,3 +9,11 @@ require("lspsaga").setup({
 })
 
 vim.keymap.set("n", "<leader>lo", ":Lspsaga outline <cr>", { desc = "Open outline" })
+vim.keymap.set({ "n", "v" }, "<leader>ca", ":Lspsaga code_action <cr>", { desc = "Run code action" })
+vim.keymap.set({ "n", "v" }, "<leader>lr", ":Lspsaga rename <cr>", { desc = "Replace in Project" })
+vim.keymap.set({ "n", "v" }, "<leader>lj", ":Lspsaga diagnostic_jump_next <cr>", { desc = "Diagnostig: jump next" })
+vim.keymap.set({ "n", "v" }, "<leader>lk", ":Lspsaga diagnostic_jump_pref <cr>", { desc = "Diagnostig: jump previous" })
+vim.keymap.set({ "n", "v" }, "<leader>ld", ":Lspsaga peek_definition <cr>", { desc = "Peek definition" })
+vim.keymap.set({ "n", "v" }, "<leader>lt", ":Lspsaga peek_type_definition <cr>", { desc = "Peek type definition" })
+vim.keymap.set({ "n", "v" }, "<leader>lf", ":Lspsaga finder <cr>", { desc = "Open Lspsaga finder" })
+vim.keymap.set({ "n", "v" }, "<leader>lm", ":Lspsaga hover_doc <cr>", { desc = "Open hover docs" })
