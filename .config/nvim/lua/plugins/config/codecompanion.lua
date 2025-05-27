e482fb6bafdbba4d2a0a2bd3e3aae7557e5619ea
Fred <fred.mny@gmal.com>
2025-05-27 10:33:46 -0300

    nvim: update CodeCompanion configuration

--- .config/nvim/lua/plugins/config/codecompanion.lua
+++ .config/nvim/lua/plugins/config/codecompanion.lua
@@ -22,6 +22,29 @@ require("codecompanion").setup({
     end,
   },
   display = {
+    chat = {
+      window = {
+        layout = "vertical", -- float|vertical|horizontal|buffer
+        position = "right", -- left|right|top|bottom (nil will default depending on vim.opt.splitright|vim.opt.splitbelow)
+        border = "single",
+        height = 0.8,
+        width = 0.45,
+        relative = "editor",
+        full_height = true,
+        opts = {
+          breakindent = true,
+          cursorcolumn = false,
+          cursorline = false,
+          foldcolumn = "0",
+          linebreak = true,
+          list = false,
+          numberwidth = 1,
+          signcolumn = "no",
+          spell = false,
+          wrap = true,
+        },
+      },
+    },
     action_palette = {
       width = 95,
       height = 10,
@@ -44,3 +67,15 @@ require("codecompanion").setup({
 		},
 	},
 })
+
+vim.keymap.set({ "n", "v" }, "<leader>cc", ":CodeCompanionChat<CR>", { desc = "Chat with CodeCompanion" })
+vim.keymap.set({ "n", "v" }, "<leader>cz", ":CodeCompanionActions<CR>", { desc = "CodeCompanion Actions" })
+vim.keymap.set({ "n", "v" }, "<leader>ci", ":CodeCompanion<CR>", { desc = "CodeCompanion Inline" })
+vim.keymap.set("v", "<leader>ce", function() require("codecompanion").prompt("cw") end, {noremap = true, silent = true, desc = "Use workflow to guide LLM" })
+vim.keymap.set("v", "<leader>ce", function() require("codecompanion").prompt("explain") end, {noremap = true, silent = true, desc = "Explain" })
+vim.keymap.set("v", "<leader>cf", function() require("codecompanion").prompt("fix") end, {noremap = true, silent = true, desc = "Fix" })
+vim.keymap.set("v", "<leader>cl", function() require("codecompanion").prompt("lsp") end, {noremap = true, silent = true, desc = "Explain LSP diagnostics" })
+-- vim.keymap.set("v", "<leader>cd", function() require("codecompanion").prompt("docs") end, {noremap = true, silent = true, desc = "GenerateDocs" })
+vim.keymap.set("v", "<leader>ct", function() require("codecompanion").prompt("tests") end, {noremap = true, silent = true, desc = "Generate Tests" })
+vim.keymap.set("n", "<leader>cm", function() require("codecompanion").prompt("commit") end, {noremap = true, silent = true, desc = "Write commit message" })
+-- vim.keymap.set("v", "<leader>cm", ":CopilotChatCreateOnComment<CR>", { desc = "Create on Comment" })
