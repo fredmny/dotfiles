60a0e2b3fb45f7dbb50ed042e3cf813ca66770a7
Fred <fred.mny@gmal.com>
2025-12-01 13:55:57 -0300

    Fix Octo config and update oil.nvim shortctu

--- .config/nvim/lua/plugins/config/oil.lua
+++ .config/nvim/lua/plugins/config/oil.lua
@@ -9,7 +9,7 @@ require("oil").setup({
     ["<C-s>"] = { "actions.select", opts = { vertical = true } },
     ["<C-h>"] = { "actions.select", opts = { horizontal = true } },
     ["<C-t>"] = { "actions.select", opts = { tab = true } },
-    ["<C-o>"] = "actions.preview",
+    ["<C-e>"] = "actions.preview",
     ["<C-p>"] = false, -- Deactivating the default behavior for this bind
     ["<C-c>"] = { "actions.close", mode = "n" },
     ["<C-l>"] = "actions.refresh",
