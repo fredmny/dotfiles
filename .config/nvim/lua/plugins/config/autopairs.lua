d1b011ecd3bff5b1f5676f6099ca4b0c3fe6a397
Fred <fred.mny@gmal.com>
2026-02-07 15:40:18 -0300

    nvim: add autopairs rule to enable triple backticks

--- .config/nvim/lua/plugins/config/autopairs.lua
+++ .config/nvim/lua/plugins/config/autopairs.lua
@@ -35,6 +35,24 @@ npairs.add_rules({
 	Rule("$", "$", "lua"):with_pair(ts_conds.is_not_ts_node({ "function" })),
 })
 
+-- When typing ```, add closing ``` on a new line (like Python docstrings)
+-- Also suppress the default single backtick pairing for the 3rd backtick
+
+npairs.get_rule("`"):with_pair(function(opts)
+	return opts.line:sub(1, opts.col):match("``$") == nil
+end)
+
+npairs.add_rules({
+	Rule("```", "```", { "-vim" })
+		:with_pair(function(opts)
+			return opts.line:sub(1, opts.col):match("``$") ~= nil
+		end)
+		:set_end_pair_length(0)
+		:replace_endpair(function()
+			return "<CR>```<Up><End>"
+		end, true),
+})
+
 -- To insert `(` after select function or method item
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 local cmp = require('cmp')
